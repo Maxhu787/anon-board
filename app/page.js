7fb17032cd95e89cdd87260c48f66860f435419b
Hu Kaixiang <maxhu787@gmail.com>
2025-06-29 19:06:22 +0800

    e

--- app/page.js
+++ app/page.js
@@ -67,7 +67,7 @@ export default function Home() {
   return (
     <div className="p-8">
       {/* <h1 className="text-2xl font-bold mb-4">Latest Posts</h1> */}
-      <UserGreetText />
+      {/* <UserGreetText /> */}
       <ul className="mb-8 flex flex-col items-center space-y-2">
         {posts.map((post) => (
           <li key={post.id} className="w-full max-w-xl">
