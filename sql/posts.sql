b6aa4d5a20f3508758bc4f52d78cac518b2860b4
Hu Kaixiang <maxhu787@gmail.com>
2025-07-01 02:11:35 +0800

    created posts sql schema

--- sql/posts.sql
+++ sql/posts.sql
@@ -1,25 +1,26 @@
-create table public.posts (
-  id uuid not null default gen_random_uuid() primary key,
-  user_id uuid references auth.users(id) on delete set null,
-  content text not null,
-  created_at timestamptz not null default now(),
-  is_anonymous boolean not null default false
-);
+create table 
+  public.posts (
+    id uuid not null default gen_random_uuid() primary key,
+    user_id uuid references auth.users(id) on delete set null,
+    content text not null,
+    created_at timestamptz not null default now(),
+    is_anonymous boolean not null default false
+  );
 
 alter table public.posts enable row level security;
 
-create policy "Anyone can select posts" on public.posts
-  for select
+create policy "Anyone can view posts." on posts for
+select
   using (true);
 
-create policy "Users can insert their own posts" on public.posts
-  for insert
-  with check (auth.uid() = user_id);
+create policy "Users can insert their own posts." on public.posts for insert
+with
+  check (auth.uid() = user_id);
 
-create policy "Users can update their own posts" on public.posts
-  for update
+create policy "Users can update their own posts." on public.posts 
+for update
   using (auth.uid() = user_id);
 
-create policy "Users can delete their own posts" on public.posts
-  for delete
+create policy "Users can delete their own posts." on public.posts 
+for delete
   using (auth.uid() = user_id);
