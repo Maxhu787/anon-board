e4d4e0ec37a4699e5bfcceedb0cb406c0281cba6
Hu Kaixiang <maxhu787@gmail.com>
2025-07-11 10:08:02 +0800

    new landing page

--- app/page.js
+++ app/page.js
@@ -45,16 +45,16 @@ export default function Home() {
     <div className="flex flex-col min-h-screen">
       <main className="flex flex-col md:flex-row flex-grow w-full">
         <section className="w-full md:w-1/2 p-8 flex items-center justify-center bg-white dark:bg-black">
-          <div className="space-y-5 md:space-y-7 max-w-xl text-left pt-25 md:pt-18 md:pl-12 md:pr-8">
+          <div className="space-y-5 md:space-y-7 max-w-xl text-left pt-25 md:pt-18 md:pl-12 md:pr-0">
             <div className="inline-block bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-200 text-xs font-medium px-3 py-1 rounded-full mb-3">
               {t("madeby")}
             </div>
-            <h1 className="text-[46px] md:text-[52px] font-extrabold leading-[50px] md:leading-[50px] text-black dark:text-white">
+            <h1 className="text-[46px] md:text-[52px] font-extrabold leading-[50px] md:leading-[50px] text-black dark:text-blue-400">
               {t("title1")}
             </h1>
-            <h1 className="text-[46px] md:text-[52px] font-extrabold leading-[50px] md:leading-[50px] mt-[-10] text-black dark:text-white">
+            <h2 className="text-[39px] md:text-[50px] font-extrabold leading-[50px] md:leading-[50px] mt-[-10] text-black dark:text-white">
               {t("title2")}
-            </h1>
+            </h2>
             <p className="text-lg md:text-[18px] text-gray-700 dark:text-gray-300">
               <span className="font-semibold">g4o2.me</span> {t("description1")}
             </p>
